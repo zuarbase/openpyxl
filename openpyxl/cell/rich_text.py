d67f8de393498480eec06d638ae27a186ea4d718
Udi Finkelstein <github@udifink.com>
2021-11-16 00:59:16 +0200

    We can now initialize a CellRichText class from an XML node, and we have the tests to prove that

--- openpyxl/cell/rich_text.py
+++ openpyxl/cell/rich_text.py
@@ -4,61 +4,22 @@
 RichText definition
 """
 
-#from openpyxl.descriptors.serialisable import Serialisable
-#from openpyxl.descriptors import (
-#    Alias,
-#    Typed,
-#    Integer,
-#    Set,
-#    NoneSet,
-#    Bool,
-#    String,
-#    Sequence,
-#)
-#from openpyxl.descriptors.nested import (
-#    NestedBool,
-#    NestedInteger,
-#    NestedString,
-#    NestedText,
-#)
-from openpyxl.styles.fonts import Font
+from openpyxl.cell.text import InlineFont
+from openpyxl.xml.functions import Element
 
-#
-# Probably not necessary to use Serializable
-#
 
-#
-# TextBlock() based on Serialisable, not needed so far.
-#
-
-#class TextBlock(Serialisable):
-#
-#    #tagname = "RElt"
-#
-#    font = Typed(expected_type=Font, allow_none=False)
-#    text = NestedText(expected_type=str, allow_none=True)
-#
-#    __elements__ = ('font', 'text')
-#
-#    def __init__(self,
-#                font=None,
-#                text=None,
-#                ):
-#        self.font = font
-#        self.text = text
-
-#
-# Pure version of TextBlock
-#
 class TextBlock:
 
     def __init__(self, font=None, text=None):
-        if not isinstance(font, Font):
-            raise TypeError("Value must be a Font class")
+        if not isinstance(font, InlineFont):
+            raise TypeError("Value must be an InlineFont class")
         self.font = font
         if not isinstance(text, str):
             raise TypeError("Value must be a string")
         self.text = text
+
+    def __repr__(self):
+        return ''.join(("InlineFont=", repr(self.font), "Text=", self.text))
 
 # Behaves as a list, but can be initialized with1 or more elements
 class CellRichText(list):
@@ -79,3 +40,31 @@ class CellRichText(list):
             else:
                 s.append(getattr(i, 'text'))
         return ''.join(s)
+
+    @classmethod
+    def tagmatch(cls, node, tag):
+        # ignore namespaces, if there are any
+        return node.tag == tag or node.tag.endswith('}'+tag)
+
+    @classmethod
+    def from_tree(cls, node):
+        # node.tag is 'si' or 'is'
+        if CellRichText.tagmatch(node[0], 't'):
+            return node[0].text # a string indicates no rich text
+        s = CellRichText()
+        for e in list(node):
+            if not CellRichText.tagmatch(e, 'r'):
+                break
+            e0 = e[0]
+            if CellRichText.tagmatch(e0, 't'):
+                s.append(e0.text)
+                continue
+            e1 = e[1]
+            if not CellRichText.tagmatch(e0, 'rPr') or not CellRichText.tagmatch(e1, 't'):
+                break
+            s.append(TextBlock(font=InlineFont.from_tree(e0), text=e1.text))
+        else:
+            return s
+        raise TypeError("unknown tag {} in OOXML file rich text string".format(e.tag))
+
+
