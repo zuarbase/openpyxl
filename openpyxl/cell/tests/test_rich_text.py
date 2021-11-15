adb7d18a3d98c3b102744ceb4ddee065999b2a05
Udi Finkelstein <github@udifink.com>
2021-11-16 00:59:16 +0200

    We can now initialize a CellRichText class from an XML node, and we have the tests to prove that

--- openpyxl/cell/tests/test_rich_text.py
+++ openpyxl/cell/tests/test_rich_text.py
@@ -6,7 +6,10 @@ import pytest
 # package imports
 
 from openpyxl.cell.rich_text import TextBlock, CellRichText
-from openpyxl.styles.fonts import Font
+from openpyxl.cell.text import InlineFont
+from openpyxl.styles.colors import Color
+
+import xml.etree.ElementTree as ET
 
 def test_rich_text_create_single():
     text = CellRichText("ABC")
@@ -17,15 +20,31 @@ def test_rich_text_create_multi():
     assert len(text) == 3
 
 def test_rich_text_create_text_block():
-    text = CellRichText(TextBlock(font=Font(), text="ABC"))
+    text = CellRichText(TextBlock(font=InlineFont(), text="ABC"))
     assert getattr(text[0], "text") == "ABC"
 
 def test_rich_text_append():
     text = CellRichText()
-    text.append(TextBlock(font=Font(), text="ABC"))
+    text.append(TextBlock(font=InlineFont(), text="ABC"))
     assert getattr(text[0], "text") == "ABC"
 
 def test_rich_text_extend():
     text = CellRichText()
     text.extend(("ABC", "DEF"))
     assert len(text) == 2
+
+def test_rich_text_from_element_simple_text():
+    node = ET.fromstring("<si><t>a</t></si>")
+    assert CellRichText.from_tree(node) == "a"
+
+def test_rich_text_from_element_rich_text_only():
+    node = ET.fromstring("<si><r><t>a</t></r></si>")
+    assert CellRichText.from_tree(node) == ["a"]
+
+def test_rich_text_from_element_rich_text_only():
+    node = ET.fromstring('<si><r><rPr><b/><sz val="11"/><color theme="1"/><rFont val="Calibri"/><family val="2"/><scheme val="minor"/></rPr><t>c</t></r></si>')
+    assert repr(CellRichText.from_tree(node)) == repr([TextBlock(font=InlineFont(sz=11, rFont="Calibri", family="2", scheme="minor", b=True, color=Color(theme=1)), text="c")])
+
+def test_rich_text_from_element_rich_text_mixed():
+    node = ET.fromstring('<si><r><t>a</t></r><r><rPr><b/><sz val="11"/><color theme="1"/><rFont val="Calibri"/><family val="2"/><scheme val="minor"/></rPr><t>c</t></r><r><t>e</t></r></si>')
+    assert repr(CellRichText.from_tree(node)) == repr(["a", TextBlock(font=InlineFont(sz=11, rFont="Calibri", family="2", scheme="minor", b=True, color=Color(theme=1)), text="c"), "e"])
