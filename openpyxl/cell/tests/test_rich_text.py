e09a7754f1cb95e0a73e11346d329610a3eba33e
Udi Finkelstein <github@udifink.com>
2021-11-17 08:15:35 +0200

    Moved all CellRichText tests under a common class

--- openpyxl/cell/tests/test_rich_text.py
+++ openpyxl/cell/tests/test_rich_text.py
@@ -11,40 +11,42 @@ from openpyxl.styles.colors import Color
 
 import xml.etree.ElementTree as ET
 
-def test_rich_text_create_single():
-    text = CellRichText("ABC")
-    assert text[0] == "ABC"
+class TestCellRichText:
 
-def test_rich_text_create_multi():
-    text = CellRichText("ABC", "DEF", "GHI")
-    assert len(text) == 3
+    def test_rich_text_create_single(self):
+        text = CellRichText("ABC")
+        assert text[0] == "ABC"
 
-def test_rich_text_create_text_block():
-    text = CellRichText(TextBlock(font=InlineFont(), text="ABC"))
-    assert getattr(text[0], "text") == "ABC"
+    def test_rich_text_create_multi(self):
+        text = CellRichText("ABC", "DEF", "GHI")
+        assert len(text) == 3
 
-def test_rich_text_append():
-    text = CellRichText()
-    text.append(TextBlock(font=InlineFont(), text="ABC"))
-    assert getattr(text[0], "text") == "ABC"
+    def test_rich_text_create_text_block(self):
+        text = CellRichText(TextBlock(font=InlineFont(), text="ABC"))
+        assert getattr(text[0], "text") == "ABC"
 
-def test_rich_text_extend():
-    text = CellRichText()
-    text.extend(("ABC", "DEF"))
-    assert len(text) == 2
+    def test_rich_text_append(self):
+        text = CellRichText()
+        text.append(TextBlock(font=InlineFont(), text="ABC"))
+        assert getattr(text[0], "text") == "ABC"
 
-def test_rich_text_from_element_simple_text():
-    node = ET.fromstring("<si><t>a</t></si>")
-    assert CellRichText.from_tree(node) == "a"
+    def test_rich_text_extend(self):
+        text = CellRichText()
+        text.extend(("ABC", "DEF"))
+        assert len(text) == 2
 
-def test_rich_text_from_element_rich_text_only():
-    node = ET.fromstring("<si><r><t>a</t></r></si>")
-    assert CellRichText.from_tree(node) == ["a"]
+    def test_rich_text_from_element_simple_text(self):
+        node = ET.fromstring("<si><t>a</t></si>")
+        assert CellRichText.from_tree(node) == "a"
 
-def test_rich_text_from_element_rich_text_only():
-    node = ET.fromstring('<si><r><rPr><b/><sz val="11"/><color theme="1"/><rFont val="Calibri"/><family val="2"/><scheme val="minor"/></rPr><t>c</t></r></si>')
-    assert repr(CellRichText.from_tree(node)) == repr([TextBlock(font=InlineFont(sz=11, rFont="Calibri", family="2", scheme="minor", b=True, color=Color(theme=1)), text="c")])
+    def test_rich_text_from_element_rich_text_only(self):
+        node = ET.fromstring("<si><r><t>a</t></r></si>")
+        assert CellRichText.from_tree(node) == ["a"]
 
-def test_rich_text_from_element_rich_text_mixed():
-    node = ET.fromstring('<si><r><t>a</t></r><r><rPr><b/><sz val="11"/><color theme="1"/><rFont val="Calibri"/><family val="2"/><scheme val="minor"/></rPr><t>c</t></r><r><t>e</t></r></si>')
-    assert repr(CellRichText.from_tree(node)) == repr(["a", TextBlock(font=InlineFont(sz=11, rFont="Calibri", family="2", scheme="minor", b=True, color=Color(theme=1)), text="c"), "e"])
+    def test_rich_text_from_element_rich_text_only(self):
+        node = ET.fromstring('<si><r><rPr><b/><sz val="11"/><color theme="1"/><rFont val="Calibri"/><family val="2"/><scheme val="minor"/></rPr><t>c</t></r></si>')
+        assert repr(CellRichText.from_tree(node)) == repr([TextBlock(font=InlineFont(sz=11, rFont="Calibri", family="2", scheme="minor", b=True, color=Color(theme=1)), text="c")])
+
+    def test_rich_text_from_element_rich_text_mixed(self):
+        node = ET.fromstring('<si><r><t>a</t></r><r><rPr><b/><sz val="11"/><color theme="1"/><rFont val="Calibri"/><family val="2"/><scheme val="minor"/></rPr><t>c</t></r><r><t>e</t></r></si>')
+        assert repr(CellRichText.from_tree(node)) == repr(["a", TextBlock(font=InlineFont(sz=11, rFont="Calibri", family="2", scheme="minor", b=True, color=Color(theme=1)), text="c"), "e"])
