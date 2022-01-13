37c1f5c035145998b67e82428412409286f0d71e
Charlie Clark <charlie.clark@clark-consulting.eu>
2022-01-13 17:54:54 +0100

    Delegate reading to blip object.

--- openpyxl/reader/drawings.py
+++ openpyxl/reader/drawings.py
@@ -65,15 +65,9 @@ def find_images(archive, path):
     for group in drawing._group_rels:
         img_group = ImageGroup()
         img_group.anchor = group.pop(0)
-        for rel in group:
-            dep = deps[rel.embed]
-            try:
-                image = Image(BytesIO(archive.read(dep.target)))
-            except OSError:
-                msg = "The image {0} will be removed because it cannot be read".format(dep.target)
-                warn(msg)
-                continue
-            image.properties = rel.properties # need xfrm to position the image within the anchor
+        for blip in group:
+            image = blip._read(deps, archive)
+            image.properties = blip.properties # need xfrm to position the image within the anchor
             img_group.append(image)
         images.append(img_group)
 
