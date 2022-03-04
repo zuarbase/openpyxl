2fd77c9badd4f40a2a4f072c8a3bd22e800fca1b
Charlie Clark <charlie.clark@clark-consulting.eu>
2022-03-04 14:04:20 +0100

    Remove repr from tests.

--- openpyxl/cell/tests/test_rich_text.py
+++ openpyxl/cell/tests/test_rich_text.py
@@ -67,25 +67,44 @@ class TestCellRichText:
     def test_rich_text_from_element_rich_text_only_text_block(self):
         node = ET.fromstring('<si><r><rPr><b/><sz val="11"/><color theme="1"/><rFont val="Calibri"/><family val="2"/><scheme val="minor"/></rPr><t>c</t></r></si>')
         text = CellRichText(node)
-        assert repr(text) == repr(CellRichText([TextBlock(font=InlineFont(sz=11, rFont="Calibri", family="2", scheme="minor", b=True, color=Color(theme=1)), text="c")]))
+        assert text == CellRichText(
+            [TextBlock(font=InlineFont(sz=11, rFont="Calibri", family="2", scheme="minor", b=True, color=Color(theme=1)),
+                       text="c")
+             ]
+        )
 
     def test_rich_text_from_element_rich_text_mixed(self):
         node = ET.fromstring('<si><r><t>a</t></r><r><rPr><b/><sz val="11"/><color theme="1"/><rFont val="Calibri"/><family val="2"/><scheme val="minor"/></rPr><t>c</t></r><r><t>e</t></r></si>')
         text = CellRichText(node)
-        assert repr(text) == repr(CellRichText(["a", TextBlock(font=InlineFont(sz=11, rFont="Calibri", family="2", scheme="minor", b=True, color=Color(theme=1)), text="c"), "e"]))
+        assert text == CellRichText(
+            ["a",
+             TextBlock(font=InlineFont(sz=11, rFont="Calibri", family="2", scheme="minor", b=True, color=Color(theme=1)),
+                            text="c"),
+             "e"]
+        )
 
 class TestCellRichTextStr:
 
     def test_rich_text_string_rhs_index(self):
         tstr = CellRichTextStr(("ab", TextBlock(InlineFont(sz="22"), "cd"), "ef"))
-        assert repr(tstr[1:5]) == repr(CellRichTextStr(("b", TextBlock(InlineFont(sz="22"), "cd"), "e")))
+        assert tstr[1:5] == CellRichTextStr(
+            ("b",
+             TextBlock(InlineFont(sz="22"), "cd"),
+             "e")
+        )
 
     def test_rich_text_string_lhs_index_1(self):
         tstr = CellRichTextStr(("ab", TextBlock(InlineFont(sz="22"), "cd"), "ef"))
         tstr[1:5] = CellRichTextStr([''])
-        assert repr(tstr) == repr(CellRichTextStr(("af",)))
+        assert tstr == CellRichTextStr(("af",))
 
     def test_rich_text_string_lhs_index_2(self):
         tstr = CellRichTextStr(("ab", TextBlock(InlineFont(sz="22"), "cd"), "ef"))
         tstr[3:3] = CellRichTextStr([TextBlock(InlineFont(b=True), "12")])
-        assert repr(tstr) == repr(CellRichTextStr(("ab", TextBlock(InlineFont(sz="22"), "c"), TextBlock(InlineFont(b=True), "12"), TextBlock(InlineFont(sz="22"), "d"), "ef")))
+        assert tstr == CellRichTextStr(
+            ("ab",
+             TextBlock(InlineFont(sz="22"), "c"),
+             TextBlock(InlineFont(b=True), "12"),
+             TextBlock(InlineFont(sz="22"), "d"),
+             "ef")
+        )
