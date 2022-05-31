fb9a989358a6c5e01af95b22d9feb92259cc940e
Charlie Clark <charlie.clark@clark-consulting.eu>
2022-05-31 13:25:50 +0200

    Initial implementation of dedicated, non-serialisanle property types, replace type by attribute with specific type instances.

--- openpyxl/packaging/tests/test_custom.py
+++ openpyxl/packaging/tests/test_custom.py
@@ -64,7 +64,7 @@ class TestCustomDocumentProperyList:
         prop2 = CustomDocumentProperty(name="PropName2", linkTarget="ExampleName")
         prop3 = CustomDocumentProperty(name="PropName3", value=2.5)
 
-        props = CustomDocumentPropertyList(customProps=[prop1, prop2, prop3])
+        props = CustomDocumentPropertyList(property=[prop1, prop2, prop3])
 
         xml = tostring(props.to_tree())
         expected = """
@@ -108,23 +108,155 @@ class TestCustomDocumentProperyList:
         ]
 
 
-    def test_delete(self, CustomDocumentPropertyList):
-        src = """
-        <Properties xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes" xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties">
+@pytest.fixture
+def TypedPropertyList():
+    from ..custom import TypedPropertyList
+    return TypedPropertyList
+
+
+from ..custom import (
+    StringProperty,
+    IntProperty,
+    FloatProperty,
+    BoolProperty,
+    DateTimeProperty,
+    LinkProperty,
+)
+
+class TestTypedPropertyList:
+
+
+    def test_ctor(self, TypedPropertyList):
+        prop_list = TypedPropertyList()
+        assert prop_list.props == []
+
+
+    def test_len(self, TypedPropertyList):
+        prop_list = TypedPropertyList()
+        assert len(prop_list) ==  0
+
+
+    def test_string(self, TypedPropertyList):
+        prop = StringProperty(name="PropName1", value="Something")
+        prop_list = TypedPropertyList()
+        prop_list.append(prop)
+
+        tree = prop_list.to_tree()
+        expected = """<Properties xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes" xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties">
           <property name="PropName1" pid="2" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
-            <vt:filetime>2020-08-24T20:19:22Z</vt:filetime>
+            <vt:lpwstr>Something</vt:lpwstr>
           </property>
-          <property name="PropName2" pid="3" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
-            <vt:r8>2.5</vt:r8>
-          </property>
-          <property name="PropName3" pid="4" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
-            <vt:bool>true</vt:bool>
-          </property>
-        </Properties>
-        """
-        node = fromstring(src)
-        props = CustomDocumentPropertyList.from_tree(node)
+        </Properties>"""
 
-        props.delete("PropName2")
+        xml = tostring(tree)
+        diff = compare_xml(xml, expected)
 
-        assert len(props.customProps) == 2
+        assert diff is None, diff
+
+
+    def test_int(self, TypedPropertyList):
+        prop = FloatProperty(name="PropName1", value=15)
+        prop_list = TypedPropertyList()
+        prop_list.append(prop)
+
+        tree = prop_list.to_tree()
+        expected = """<Properties xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes" xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties">
+          <property name="PropName1" pid="2" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
+            <vt:r8>15</vt:r8>
+          </property>
+        </Properties>"""
+
+        xml = tostring(tree)
+        diff = compare_xml(xml, expected)
+
+        assert diff is None, diff
+
+
+    def test_float(self, TypedPropertyList):
+        prop = IntProperty(name="PropName1", value=15)
+        prop_list = TypedPropertyList()
+        prop_list.append(prop)
+
+        tree = prop_list.to_tree()
+        expected = """<Properties xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes" xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties">
+          <property name="PropName1" pid="2" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
+            <vt:i4>15</vt:i4>
+          </property>
+        </Properties>"""
+
+        xml = tostring(tree)
+        diff = compare_xml(xml, expected)
+
+        assert diff is None, diff
+
+
+    def test_bool(self, TypedPropertyList):
+        prop = BoolProperty(name="PropName1", value=False)
+        prop_list = TypedPropertyList()
+        prop_list.append(prop)
+
+        tree = prop_list.to_tree()
+        expected = """<Properties xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes" xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties">
+          <property name="PropName1" pid="2" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
+            <vt:bool>0</vt:bool>
+          </property>
+        </Properties>"""
+
+        xml = tostring(tree)
+        diff = compare_xml(xml, expected)
+
+        assert diff is None, diff
+
+
+    def test_datetime(self, TypedPropertyList):
+        prop = DateTimeProperty(name="PropName1", value=datetime.datetime(2022, 5, 31, 12, 55, 13))
+        prop_list = TypedPropertyList()
+        prop_list.append(prop)
+
+        tree = prop_list.to_tree()
+        expected = """<Properties xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes" xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties">
+          <property name="PropName1" pid="2" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
+            <vt:filetime>2022-05-31T12:55:13Z</vt:filetime>
+          </property>
+        </Properties>"""
+
+        xml = tostring(tree)
+        diff = compare_xml(xml, expected)
+
+        assert diff is None, diff
+
+
+    def test_link(self, TypedPropertyList):
+        prop = LinkProperty(name="PropName1", value="A link")
+        prop_list = TypedPropertyList()
+        prop_list.append(prop)
+
+        tree = prop_list.to_tree()
+        expected = """<Properties xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes" xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties">
+          <property name="PropName1" pid="2" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" linkTarget="A link">
+            <vt:lpwstr/>
+          </property>
+        </Properties>"""
+
+        xml = tostring(tree)
+        diff = compare_xml(xml, expected)
+
+        assert diff is None, diff
+
+
+    def test_names(self, TypedPropertyList):
+        prop1 = StringProperty(name="PropName1", value="Something")
+        prop2 = LinkProperty(name="PropName2", value="A link")
+        prop_list= TypedPropertyList()
+        prop_list.props = [prop1, prop2]
+        assert prop_list.names == ["PropName1", "PropName2"]
+
+
+    def test_duplicate(self, TypedPropertyList):
+        prop1 = StringProperty(name="PropName1", value="Something")
+        prop2 = LinkProperty(name="PropName1", value="A link")
+        prop_list= TypedPropertyList()
+        prop_list.props = [prop1]
+        with pytest.raises(ValueError):
+            prop_list.append(prop2)
+
