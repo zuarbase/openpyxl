0465a0e52de9d5dbf98b8b676356a0fc5f22694d
snippet <liamsandersliam@gmail.com>
2022-08-08 08:22:24 +0000

    Allow Image to optinally have alt text specified

--- openpyxl/drawing/tests/test_spreadsheet_drawing.py
+++ openpyxl/drawing/tests/test_spreadsheet_drawing.py
@@ -280,7 +280,32 @@ class TestSpreadsheetDrawing:
         <pic xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main"
           xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
           <nvPicPr>
-            <cNvPr descr="Picture" id="4" name="Image 4"></cNvPr>
+            <cNvPr id="4" name="Image 4"></cNvPr>
+            <cNvPicPr />
+          </nvPicPr>
+          <blipFill>
+            <a:blip cstate="print" r:embed="rId4" />
+            <a:stretch xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main">
+              <a:fillRect/>
+            </a:stretch>
+          </blipFill>
+          <spPr>
+            <a:prstGeom prst="rect" />
+          </spPr>
+        </pic>
+        """
+        diff = compare_xml(xml, expected)
+        assert diff is None, diff
+
+    def test_write_picture_with_alttext(self, SpreadsheetDrawing):
+        drawing = SpreadsheetDrawing()
+        pic = drawing._picture_frame(4, "I have set a desc")
+        xml = tostring(pic.to_tree())
+        expected = """
+        <pic xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main"
+          xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+          <nvPicPr>
+            <cNvPr descr="I have set a desc" id="4" name="Image 4"></cNvPr>
             <cNvPicPr />
           </nvPicPr>
           <blipFill>
